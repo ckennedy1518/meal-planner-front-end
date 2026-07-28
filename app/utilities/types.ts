f64f8877ca21489570d444407fff331107dfd68e
Christopher Kennedy <ckennedy22@wisc.edu>
2026-07-28 16:03:50 -0500

    f

--- app/utilities/types.ts
+++ app/utilities/types.ts
@@ -1,8 +1,16 @@
+export type Unit = 'Tbsp' | 'tsp' | 'C' | 'oz' | 'fl oz' | 'lbs' | 'mL' | 'g';
 export type Mode = 'cook' | 'pantry' | 'plan' | 'settings' | null;
 export type Recipe = {
     name: string;
-    ingredients: string[];
+    ingredients: Ingredient[];
     instructions: string[];
     datePlanned: Date | null;
     lastMade: Date | null;
+};
+export type Ingredient = {
+    name: string;
+    quantity: number;
+    unit: Unit;
+    secondaryQuantity: number | undefined; // e.g. 1 lb 10 oz
+    secondaryUnit: Unit | undefined;
 };
