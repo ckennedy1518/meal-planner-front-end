d300dc4e8fed3f1e75d599df442803e147f9753b
Christopher Kennedy <ckennedy22@wisc.edu>
2026-08-05 14:42:04 -0500

    feat: Edit mode in IngredientDisplay

--- app/components/IngredientDisplay.tsx
+++ app/components/IngredientDisplay.tsx
@@ -1,18 +1,74 @@
-import { GroceryListIngredient, Ingredient } from '../utilities/types';
+import { useCallback } from 'react';
+import { Pressable, Text, TextInput } from 'react-native';
+import { Dropdown } from 'react-native-element-dropdown';
+import { useMealPlannerStore } from '../state/useMealPlannerStore';
+import {
+    GroceryListIngredient,
+    Ingredient,
+    Unit,
+    unitValues
+} from '../utilities/types';
 import { ThemedText } from './ThemedText';
 
 interface IIngredientDisplay {
     ingredient: Ingredient | GroceryListIngredient;
+    isInEditMode?: boolean;
 }
 
 export function IngredientDisplay(
     props: IIngredientDisplay
 ): React.JSX.Element {
     const ingredient: Ingredient | GroceryListIngredient = props.ingredient;
+    const isInEditMode: boolean = props.isInEditMode ?? false;
+    const unitOptions = unitValues.map((unit) => ({
+        label: unit,
+        value: unit
+    }));
 
-    let formattedString = `Ingredient info - Name: ${ingredient.name}; Quantity: ${ingredient.quantity}; Unit: ${ingredient.unit}; isStaple: ${ingredient.isStaple}`;
-    if (ingredient.hasOwnProperty('quantity_purchased')) {
-        formattedString += `; Quantity Purchased: ${(ingredient as GroceryListIngredient).quantity_purchased}`;
-    }
-    return <ThemedText>{formattedString}</ThemedText>;
+    const onChangeText = (text: string) => {
+        ingredient.quantity = parseFloat(text.replace(/[^0-9.]/g, '')) || 0;
+        useMealPlannerStore.getState().addEditingIngredient(ingredient);
+    };
+
+    const onChangeUnit = (unit: string) => {
+        ingredient.unit = unit as Unit;
+        useMealPlannerStore.getState().addEditingIngredient(ingredient);
+    };
+
+    const onRemoveIngredient = useCallback(() => {
+        useMealPlannerStore.getState().addRemovingIngredient(ingredient);
+    }, []);
+
+    return (
+        <>
+            {isInEditMode ? (
+                <>
+                    <TextInput
+                        style={{}}
+                        keyboardType="numeric"
+                        onChangeText={onChangeText}
+                        value={ingredient.quantity.toString()}
+                    />
+                    <Dropdown
+                        data={unitOptions}
+                        labelField="label"
+                        valueField="value"
+                        value={ingredient.unit}
+                        onChange={onChangeUnit}
+                    />
+                </>
+            ) : (
+                <>
+                    <ThemedText>{ingredient.quantity}</ThemedText>
+                    <ThemedText>{ingredient.unit}</ThemedText>
+                </>
+            )}
+            <ThemedText>{ingredient.name}</ThemedText>
+            {isInEditMode && (
+                <Pressable onPress={onRemoveIngredient}>
+                    <Text>Remove</Text>
+                </Pressable>
+            )}
+        </>
+    );
 }
--- app/state/createPantrySlice.ts
+++ app/state/createPantrySlice.ts
@@ -5,17 +5,30 @@ import { GroceryList, Ingredient } from '../utilities/types';
 export interface IPantrySlice {
     allIngredients: Ingredient[];
     groceryLists: GroceryList[];
+    addingIngredients: Ingredient[];
+    editingIngredients: Ingredient[];
+    removingIngredients: Ingredient[];
     addIngredient: (ingredient: Ingredient) => void;
     removeIngredient: (
         ingredient: Ingredient,
         shouldRemoveEntirely: boolean
     ) => void;
     addGroceryList: (groceryList: GroceryList) => void;
+    addAddingIngredient: (ingredient: Ingredient) => void;
+    removeAddingIngredient: (ingredient: Ingredient) => void;
+    addEditingIngredient: (ingredient: Ingredient) => void;
+    removeEditingIngredient: (ingredient: Ingredient) => void;
+    addRemovingIngredient: (ingredient: Ingredient) => void;
+    removeRemovingIngredient: (ingredient: Ingredient) => void;
+    resetChanges: () => void;
 }
 
 export const createPantrySlice: StateCreator<IPantrySlice> = (set) => ({
     allIngredients: [],
     groceryLists: [],
+    addingIngredients: [],
+    editingIngredients: [],
+    removingIngredients: [],
     addIngredient: (ingredient: Ingredient) =>
         set((state) => {
             const doesIngredientExist = state.allIngredients.find(
@@ -60,5 +73,56 @@ export const createPantrySlice: StateCreator<IPantrySlice> = (set) => ({
     addGroceryList: (groceryList: GroceryList) =>
         set((state) => ({
             groceryLists: [...state.groceryLists, groceryList]
+        })),
+    addAddingIngredient: (ingredient: Ingredient) =>
+        set((state) => ({
+            addingIngredients: [...state.addingIngredients, ingredient]
+        })),
+    removeAddingIngredient: (ingredient: Ingredient) =>
+        set((state) => ({
+            addingIngredients: state.addingIngredients.filter(
+                (i) => i.name !== ingredient.name
+            )
+        })),
+    addEditingIngredient: (ingredient: Ingredient) =>
+        set((state) => {
+            if (
+                state.editingIngredients.find((i) => i.name === ingredient.name)
+            ) {
+                return {
+                    editingIngredients: state.editingIngredients.map((i) =>
+                        i.name === ingredient.name ? ingredient : i
+                    )
+                };
+            } else {
+                return {
+                    editingIngredients: [
+                        ...state.editingIngredients,
+                        ingredient
+                    ]
+                };
+            }
+        }),
+    removeEditingIngredient: (ingredient: Ingredient) =>
+        set((state) => ({
+            editingIngredients: state.editingIngredients.filter(
+                (i) => i.name !== ingredient.name
+            )
+        })),
+    addRemovingIngredient: (ingredient: Ingredient) =>
+        set((state) => ({
+            removingIngredients: [...state.removingIngredients, ingredient]
+        })),
+    removeRemovingIngredient: (ingredient: Ingredient) =>
+        set((state) => ({
+            removingIngredients: state.removingIngredients.filter(
+                (i) => i.name !== ingredient.name
+            )
+        })),
+    resetChanges: () =>
+        set((state) => ({
+            addingIngredients: [],
+            editingIngredients: [],
+            removingIngredients: []
         }))
 });
